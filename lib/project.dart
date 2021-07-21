08428fcec77206054e797b963b596ef494ccce48
Yogesh Kumar <yogeshk4124@gmail.com>
2021-07-22 00:08:34 +0530

    project class added

---
+++ lib/project.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class projects {
+  DateTime? date;
+  String? title, summary;
+  String? imgsrc;
+
+  projects(
+      {@required this.date,
+      @required this.title,
+      @required this.summary,
+      @required this.imgsrc});
+}
